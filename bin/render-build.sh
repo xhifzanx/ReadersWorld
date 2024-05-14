7216c9b97c2886924efa1703e9caa0f1038870e7
xhifzanx <misterj435@gmail.com>
2024-05-14 19:00:15 +0530

    render build changes

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,8 @@
 # exit on error
 set -o errexit
 
+bundle config set without development test
+bundle config set force_ruby_platform true # https://stackoverflow.com/a/66311533
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
